3db0af1c1417a85c275eed46025bd4109e31b9e6
Chmouel Boudjnah <chmouel.boudjnah@rackspace.co.uk>
2011-02-17 17:10:39 +0000

    Make it works.

--- guestbook/cron.py
+++ guestbook/cron.py
@@ -10,6 +10,17 @@ import os
 import re
 import sys
 import time
+import optparse
+
+SET_CRON_HELP="""Specify a start and a End time. The format can be :
+15h34
+15:34
+2 minute
+5 hours
+10 seconds
+
+Please allow at least 10 minutes between start and end time.
+"""
 
 sys.path.append(
     os.path.abspath(
@@ -24,20 +35,63 @@ MEMCACHED_SERVERS=["localhost:11211"]
 
 TYPE_NOT_SETUP=-1
 TYPE_NOTHING=0
-TYPE_START=1
-TYPE_END=2
-TYPE_ALREADY_CREATED=3
-TYPE_NOT_ALREADY_CREATED=4
+
+TYPE_BACKUP=1
+TYPE_RECOVER=2
+TYPE_CANNOT_BACKUP=3
+TYPE_CANNOT_RECOVER=4
 
 SERVER_NAMES=['demo-web1', 'demo-db1']
 BACKUP_PREFIX="backup"
 IMAGE_DEFAULT_FLAVOR_ID=1
 
+def timesince(d, now=None):
+    def ppr(s, p, n):
+        if n == 1: return s
+        return p
+    chunks = (
+      (60 * 60 * 24 * 365, lambda n: ppr('year', 'years', n)),
+      (60 * 60 * 24 * 30, lambda n: ppr('month', 'months', n)),
+      (60 * 60 * 24, lambda n : ppr('day', 'days', n)),
+      (60 * 60, lambda n: ppr('hour', 'hours', n)),
+      (60, lambda n: ppr('minute', 'minutes', n))
+    )
+    if now:
+        t = now.timetuple()
+    else:
+        t = time.localtime()
+    tz = None
+    now = datetime.datetime(t[0], t[1], t[2], t[3], t[4], t[5], tzinfo=tz)
+    delta = now - d
+    since = delta.days * 24 * 60 * 60 + delta.seconds
+    for i, (seconds, name) in enumerate(chunks):
+        count = since / seconds
+        if count != 0:
+            break
+    s = '%d %s' % (count, name(count))
+    if i + 1 < len(chunks):
+        # Now get the second item
+        seconds2, name2 = chunks[i + 1]
+        count2 = (since - (seconds * count)) / seconds2
+        if count2 != 0:
+            s += ', %d %s' % (count2, name2(count2))
+
+    if s == "0 minutes":
+        s ="about a minute... (checking)"
+        
+    return s
+
+def timeuntil(d):
+    now = datetime.datetime.now()
+    return timesince(now, d)
+
 class ScheduledTask(object):
     def __init__(self):
         self.mclient = memcache.Client(MEMCACHED_SERVERS, debug=0)
         self.cnx = None
-
+        self.server_list = None
+        self.sleep_time = 15
+        
     def setup_log(self, ttype, maxBytes=None, backupCount=7):
         maxBytes = 1024 * 1024 
         date_fmt = '%m/%d/%Y %H:%M:%S'
@@ -94,8 +148,8 @@ class ScheduledTask(object):
 
     def is_server_created(self):
         self.get_cnx()
-        server_list = self.cnx.servers.list()
-        server_list_names = [s.name for s in server_list ]
+        self.server_list = self.cnx.servers.list()
+        server_list_names = [s.name for s in self.server_list ]
         lock=False
         for server in SERVER_NAMES:
             if server in server_list_names:
@@ -109,7 +163,13 @@ class ScheduledTask(object):
             status = ftype.get(image_id).status
             if status != previous_status:
                 previous_status = status
+                if status == "UNKNOWN":
+                    continue
                 self.mclient.set("demo-cron-%sStatus" % (ttype), status)
+                if self.options.verbose:
+                    self.ssprint("%s " % (status))
+            else:
+                self.ssprint(".")
             if status == "ACTIVE":
                 self.mclient.delete("demo-cron-%sStatus" % (ttype))
                 return
@@ -117,8 +177,9 @@ class ScheduledTask(object):
     
     def do_backup(self, delete_server_after=True):
         self.get_cnx()
-        server_list = self.cnx.servers.list()
-        for server in server_list:
+        if not self.server_list:
+            self.server_list = self.cnx.servers.list()
+        for server in self.server_list:
             if server.name in SERVER_NAMES:
                 backup_image_name = "%s-%s" % (BACKUP_PREFIX, server.name)
                 backuping = self.mclient.get("demo-cron-backupCurrentImage")
@@ -127,6 +188,7 @@ class ScheduledTask(object):
                     return
                 self.mclient.set("demo-cron-backupCurrentImage", server.name)
                 self.mclient.set("demo-cron-backupFlavorId%s" % (str(server.name)), server.flavorId)
+                self.ssprint("[%s] " % (server.name))
                 backup_image = self.cnx.images.create(backup_image_name, server.id)
                 self.logAndWait(backup_image.id, self.cnx.images, "backup")
 
@@ -167,6 +229,8 @@ class ScheduledTask(object):
                 else:
                     fId = IMAGE_DEFAULT_FLAVOR_ID
 
+                self.ssprint("[%s] " % (sname))
+                
                 self.mclient.set("demo-cron-restoreCurrentImage", sname)                
                 cs = self.cnx.servers.create(
                     image=image.id,
@@ -206,36 +270,8 @@ class ScheduledTask(object):
         if not bCi:
             print "Nothing going on"
             return
-        print "%s:%s" % (self.mclient.get("demo-cron-%sCurrentImage" % (ttype)),
+        return "%s:%s" % (self.mclient.get("demo-cron-%sCurrentImage" % (ttype)),
                                        self.mclient.get("demo-cron-%sStatus" % (ttype)))
-
-    def check_if_need_to_run(self, start=None, end=None, now=None):
-        if not start:
-            mstart=self.mclient.get("demo-cron-start")
-            if mstart:
-                start = datetime.datetime.fromtimestamp(float(mstart))
-            else:
-                return TYPE_NOT_SETUP
-        if not end:
-            mend = self.mclient.get("demo-cron-end")
-            if mend:
-                end = datetime.datetime.fromtimestamp(float(mend))
-            else:
-                return TYPE_NOT_SETUP
-        if not now:
-            now = datetime.datetime.now()
-
-        if now > start and now >= end:
-            iscreated = self.is_server_created()
-            if not iscreated:
-                return TYPE_NOT_ALREADY_CREATED
-            return TYPE_END
-        elif now >= start:
-            iscreated = self.is_server_created()
-            if iscreated:
-                return TYPE_ALREADY_CREATED
-            return TYPE_START
-        return TYPE_NOTHING
 
     def testit(self):
         dtn = datetime.datetime.now()
@@ -260,12 +296,12 @@ class ScheduledTask(object):
         self.check_if_need_to_run(start=start, end=end, now=now)
             
     def set_cron(self):
-        self.help()
+        print SET_CRON_HELP
         dtn = datetime.datetime.now()
 
         start_time = self.ask_for_time('Start time: ')
         if dtn  > start_time:
-            print "Cannot set a time in the past of this hour"
+            print "Cannot set a time in the past."
             return
         self.mclient.set("demo-cron-start", time.mktime(start_time.timetuple()))
         
@@ -275,36 +311,165 @@ class ScheduledTask(object):
             return
         self.mclient.set("demo-cron-end", time.mktime(end_time.timetuple()))
         
-    def help(self):
-        print """Specify a start and a End time. The format can be :
-15h34
-15:34
-2 minute
-5 hours
-10 seconds
+    def main(self):
+        opparser = optparse.OptionParser(usage="cron.py [options]")
+        opparser.add_option('-b', '--backup',
+                            dest="backup",
+                            action="store_true",
+                            help="Launch Backup"
+                            )
 
-Please allow at least 10 minutes between start and end time.
-"""
+        opparser.add_option('-r', '--restore',
+                            dest="restore",
+                            action="store_true",
+                            help="Launch Restore"
+                            )
 
+        opparser.add_option('-c', '--set-cron',
+                            dest="setcron",
+                            action="store_true",
+                            help="Set Cron."
+                            )
+
+        opparser.add_option('--backup-status',
+                            dest="backupStatus",
+                            action="store_true",
+                            help="Get backup status "
+                            )
+
+        opparser.add_option('--restore-status',
+                            dest="restoreStatus",
+                            action="store_true",
+                            help="Get restore status "
+                            )
+
+        opparser.add_option('-k', '--cleanup',
+                            dest="cleanup",
+                            action="store_true",
+                            help="Cleanup all locks session status."
+                            )
+
+        opparser.add_option('-K', '--hard-cleanup',
+                            dest="hard_cleanup",
+                            action="store_true",
+                            help="Will cleanup everything including servers and images."
+                            )
+        
+        opparser.add_option('-d', '--daemon',
+                            dest="daemon",
+                            action="store_true",
+                            help="Launch cron daemons."
+                            )
+
+        opparser.add_option('-v', '--verbose',
+                            dest="verbose",
+                            action="store_true",
+                            help="Be verbose."
+                            )
+        
+        (options, args) = opparser.parse_args() # pylint: disable-msg=W0612
+        self.options = options
+        
+        if options.restore:
+            self.do_restore()
+        elif options.backup:
+            self.do_backup()
+        elif options.setcron:
+            self.set_cron()
+        elif options.backupStatus:
+            print self.get_status("backup")
+        elif options.restoreStatus:
+            print self.get_status("restore")
+        elif options.cleanup:
+            self.cleanup()
+        elif options.hard_cleanup:
+            print "TODO: This should get implemented"
+            return
+        #self.hard_cleanup()
+        elif options.daemon:
+            #TODO: Not a proper daemon yet!
+            self.run_daemon()
+        else:
+            print "No arguments specified please see --help"
+            return
+
+    def cleanup(self):
+        print "Cleaning up:"
+        s = ["demo-cron-backupCurrentImage", "demo-cron-restoreCurrentImage", "demo-cron-restoreStatus", "demo-cron-restoreStatus", "demo-cron-end", "demo-cron-start"]
+        for t in s:
+            self.mclient.delete(t)
+
+    def ssprint(self, s):
+        if self.options.verbose:
+            sys.stdout.write(s)
+            sys.stdout.flush()
+            
+    def run_daemon(self):
+        while True:
+            mdstart=self.mclient.get("demo-cron-start")
+            mdend=self.mclient.get("demo-cron-end")
+            if not all([mdstart, mdend]):
+                print "Cron not setup yet"
+                return
+
+            i_have_printed=False
+            
+            dtstart=datetime.datetime.fromtimestamp(mdstart)
+            dtend=datetime.datetime.fromtimestamp(mdend)
+            now=datetime.datetime.now()
+            
+            nh, nm = (now.hour, now.minute)
+            sh, sm = (dtstart.hour, dtstart.minute)
+            eh, em = (dtend.hour, dtend.minute)
+            if nh == sh and nm == sm:
+                iscreated = self.is_server_created()
+                if not iscreated:
+                    doingIt = self.mclient.get("demo-cron-backupCurrentImage")
+                    if doingIt:
+                        s,t = self.get_status("backup").split(":")
+                        print "I am backuping %s status, is %s" % (s, t)
+                        continue
+                    else:
+                        if not i_have_printed:
+                            i_have_printed=True
+                            print "I was going to backup but they are already stored in images so I am going to pass on this one."
+                        time.sleep(self.sleep_time)
+                        continue
+                self.ssprint("Storing servers as images: ")
+                self.do_backup()
+                self.ssprint("Done.\n")
+            elif nh == eh and nm == em:
+                iscreated = self.is_server_created()
+                if iscreated:
+                    doingIt = self.mclient.get("demo-cron-recoverCurrentImage")
+                    if doingIt:
+                        s,t = self.get_status("recover").split(":")
+                        print "I am recovering %s, status is %s" % (s, t)
+                        continue
+                    else:
+                        print "TODO: This should not happen!"
+                        continue
+                self.ssprint("Restoring servers from backup: ")
+                self.do_restore()
+                self.ssprint("Done. Go to http://demo-web1.dyndns.info\n")
+            else:
+                if self.options.verbose:
+                    iscreated = self.is_server_created()
+                    if not iscreated and now >= dtstart:
+                        print "Will start recovering in %s" %  timeuntil(dtend)
+                    else:
+                        if now > dtstart:
+                            print "Will start backup tomorrow at %s." % dtstart.strftime("%H:%M:%S")
+                            self.sleep_time = 3600
+                        else:
+                            print "Will start backup in %s" % timeuntil(dtstart)
+                            self.sleep_time = 15
+                time.sleep(self.sleep_time)
+                
 if __name__ == '__main__':
-    c = ScheduledTask()
+    try:
+        c = ScheduledTask()
+        c.main()
+    except(KeyboardInterrupt):
+        pass
     #c.testit()
-    #c.set_cron()
-    #c.do_backup()
-    #c.do_restore()
-    #c.get_status("restore")
-    #c.get_status("backup")
-    # result = c.check_if_need_to_run()
-    # if result == TYPE_NOTHING:
-    #     print "Nothing to do for today cron."
-    # elif result == TYPE_START:
-    #     print "I am about to start"
-    # elif result == TYPE_END:
-    #     print "I am about to end"
-    # elif result == TYPE_NOT_SETUP:
-    #     print "Nothing is setup."
-    # elif result == TYPE_ALREADY_CREATED:
-    #     print "You want me to start but servers are already created"
-    # elif result == TYPE_NOT_ALREADY_CREATED:
-    #     print "You want me to end but servers are not already created"
-    
