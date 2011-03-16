87630218517fc6c78effffca61267b81abaad4d4
Chmouel Boudjnah <chmouel.boudjnah@rackspace.co.uk>
2011-03-16 17:54:53 +0000

    Allow passing arguments.

--- python/lib/commands.py
+++ python/lib/commands.py
@@ -5,7 +5,7 @@ import tempfile
 from subprocess import call
 
 
-def copy_exec_script(public_ip, script, username='root'):
+def copy_exec_script(public_ip, script, args=[], username='root'):
     common_options = ['-q',
                    '-o StrictHostKeyChecking=no',
                    '-o UserKnownHostsFile=/dev/null']
@@ -16,9 +16,10 @@ def copy_exec_script(public_ip, script, username='root'):
              (" ".join(common_options), script, username,
               public_ip, tmpname), shell=True)
 
-    call('ssh -t -x %s %s@%s "%s && rm -f %s"' %  \
+    call('ssh -t -x %s %s@%s "%s %s && rm -f %s"' %  \
              (" ".join(common_options), username,
-              public_ip, tmpname, tmpname), shell=True)
+              public_ip, tmpname, " ".join(args), tmpname), shell=True)
 
 if __name__ == '__main__':
-    copy_exec_script('IP', '/tmp/a.sh')
+    copy_exec_script(os.environ.get('IP'), '/tmp/a.sh',
+                     args=["Hello2", "Bar"])
