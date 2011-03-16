8b5a4f531c3463eedd1f6f125c79e5af4734be71
Chmouel Boudjnah <chmouel.boudjnah@rackspace.co.uk>
2011-03-16 18:18:05 +0000

    Add firewall control script.

---
+++ firewall/fw-client.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+export DEBIAN_FRONTEND=noninteractive
+
+SETUP=
+if [[ -f /etc/redhat-release ]];then
+    DISTRO="redhat"
+    if [[ ! -x /usr/bin/system-config-securitylevel-tui ]];then
+        yum -y install system-config-securitylevel-tui
+    fi
+elif [[ -x /usr/bin/lsb_release ]];then
+    DISTRO=$(lsb_release -i -s)
+    [[ ${DISTRO,,} == "ubuntu" ]] && DISTRO="debian"
+
+    if [[ ${DISTRO} == "ubuntu" && ! -x /usr/sbin/ufw ]];then
+        apt-get -y install ufw
+    fi
+
+    SETUP="1"
+fi
+
+if [[ $1 == "status" ]];then
+    if [[ ${DISTRO} == "debian" ]];then
+        ufw status numbered
+    elif [[ ${DISTRO} == "redhat" ]];then
+        iptables -L -n
+    fi
+    exit
+elif [[ $1 == disable ]];then
+    if [[ ${DISTRO} == "debian" ]];then
+        ufw -f disable
+    elif [[ ${DISTRO} == "redhat" ]];then
+        echo -n "Disabling firewall: "
+        system-config-securitylevel-tui  --disabled -q
+        echo "done."
+    fi
+    exit 0
+elif [[ $1 == enable ]];then
+    if [[ ${DISTRO} == "debian" ]];then
+        ufw allow proto tcp from any to any port 22
+        ufw -f enable
+    elif [[ ${DISTRO} == "redhat" ]];then
+        echo -n "Enabling firewall: "
+        system-config-securitylevel-tui  --enabled -q
+        echo "done."
+    fi
+    exit 0
+elif [[ $@ == allow\ port* ]];then
+    PORT=${@: -1}
+    if [[ ${DISTRO} == "debian" ]];then
+        ufw allow ${PORT}
+    elif [[ ${DISTRO} == "redhat" ]];then
+        system-config-securitylevel-tui  -p ${PORT}:tcp -q
+        echo "Rules added"
+    fi
+    exit
+elif [[ $@ == deny\ port* ]];then
+    PORT=${@: -1}
+    if [[ ${DISTRO} == "debian" ]];then
+        ufw deny ${PORT}
+    elif [[ ${DISTRO} == "redhat" ]];then
+        sed -i  "/^-A RH-Firewall.*dport ${PORT} -j ACCEPT/d" /etc/sysconfig/iptables 
+        iptables-restore < /etc/sysconfig/iptables
+        echo "Rules deleted"
+    fi
+    exit
+else
+    if [[ ${DISTRO} == "debian" ]];then
+        if [[ -z $@ ]];then
+            ufw help
+        else
+            ufw $@
+        fi
+    elif [[ ${DISTRO} == "redhat" ]];then
+        if [[ -z $@ ]];then
+            echo "I am about to launch the GUI to configure the server"
+            system-config-securitylevel-tui
+        else
+            system-config-securitylevel-tui $@
+        fi
+    fi
+fi
---
+++ firewall/fw-control.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+import sys
+import os
+from optparse import OptionParser
+
+TOPDIR = os.path.abspath(os.path.join(os.path.dirname(sys.argv[0]), ".."))
+sys.path.append(os.path.join(TOPDIR, "python"))
+
+from lib.chooseserver import ChooseServer
+from lib.common import CNX
+from lib.commands import copy_exec_script
+
+if __name__ == '__main__':
+    opparser = OptionParser(usage="delete-images")
+    opparser.add_option('-s', '--server',
+                        type='string',
+                        help="Server List id/name to match")
+    (options, args) = opparser.parse_args()
+
+    if 'help' in args:
+        print """firewall -- simple firewall wrapper to OS firewall wrapper to iptables via the Cloud.
+
+Usage :
+
+firewall allow port XX -- allow only port XX
+firewall deny port XX -- disable port XX
+firewall disable -- Allow everything!
+firewall enable -- Enable firewall
+"""
+        sys.exit(0)
+
+    c = ChooseServer(CNX)
+    if len(c.servers) == 0:
+        print "There is no images to process."
+        sys.exit(0)
+    server_list = c.get_list_of_servers(options.server)
+    if not server_list:
+        print "No server selected"
+        sys.exit(1)
+
+    for server in server_list:
+        print "Processing: %s/%s" % (server.name, server.public_ip)
+        copy_exec_script(server.public_ip,
+                         '%s/firewall/fw-client.sh' % (TOPDIR),
+                         args)
+        print
