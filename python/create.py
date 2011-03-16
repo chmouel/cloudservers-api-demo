51f2c15f713fcd3fdd39404760a9cfec98ae1987
Chmouel Boudjnah <chmouel.boudjnah@rackspace.co.uk>
2011-03-16 17:50:03 +0000

    Cleanup create script.
    Move copy_exec_script function to it's own library and cleanup some PEP8
    errors.

--- python/create.py
+++ python/create.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 import os
 import sys
-import tempfile
-from subprocess import call
 from optparse import OptionParser
 
 from lib.common import CNX
 from lib.images import Images
+from lib.commands import copy_exec_script
 
-bootstrap=True
+BOOTSTRAP = True
+
 
 def check_image(cnx, server_id):
     import time
@@ -17,18 +17,20 @@ def check_image(cnx, server_id):
 
     sys.stdout.write("[%s]" % (" " * toolbar_width))
     sys.stdout.flush()
-    sys.stdout.write("\b" * (toolbar_width+1)) # return to start of line, after '['
+    sys.stdout.write("\b" * (toolbar_width + 1))
 
     while cnx.servers.get(server_id).status != "ACTIVE":
-        status=cnx.servers.get(server_id).status
-        if status  == "ACTIVE":
+        status = cnx.servers.get(server_id).status
+        if status == "ACTIVE":
             break
         elif status == "ERROR":
             print
-            print "We have an error while building which is unfortunate, you may have to restart the process again and contact cloud support providing this id: %s"  % (server_id)
+            print "We have an error while building which is unfortunate" + \
+                ", you may have to restart the process again and contact" + \
+                " cloud support providing this id: %s" % (server_id)
             sys.exit(1)
-            
-        time.sleep(0.5) # do real work here
+
+        time.sleep(0.5)
         sys.stdout.write("-")
         sys.stdout.flush()
 
@@ -50,7 +52,7 @@ opparser.add_option('-f', '--flavor_id',
 opparser.add_option('-B', '--no-bootstrap',
                     action="store_false",
                     dest="bootstrap",
-                    default=True,
+                    default=BOOTSTRAP,
                     help="No Bootstraping basic configuration")
 
 opparser.add_option('-s', '--script',
@@ -65,23 +67,23 @@ opparser.add_option('-D', '--delete_image',
                     help="Delete image from where this is stored after creating")
 
 
-(options, args) = opparser.parse_args() # pylint: disable-msg=W0612
+(options, args) = opparser.parse_args()
 
 if options.script and not os.path.exists(options.script):
     print "%s not exist" % (options.script)
     sys.exit(1)
 
 if options.name:
-    IMAGE_NAME=options.name
+    IMAGE_NAME = options.name
 else:
     ans = raw_input("VM Name: ")
     if not ans.strip():
-      print "I need a name. exiting."
-      sys.exit(1)
-    IMAGE_NAME=ans
+        print "I need a name. exiting."
+        sys.exit(1)
+    IMAGE_NAME = ans
 
 if options.id:
-    IMAGE_TYPE=options.id
+    IMAGE_TYPE = options.id
 else:
     image = Images(CNX)
 
@@ -90,7 +92,7 @@ else:
     IMAGE_TYPE = int(raw_input("Choose an Image ID: "))
 
 if options.flavor_id:
-    IMAGE_FLAVOUR_ID=options.flavor_id
+    IMAGE_FLAVOUR_ID = options.flavor_id
 else:
     for i in CNX.flavors.list():
         print "%s) %s" % (i.id, i.name)
@@ -109,25 +111,21 @@ if options.delete_image:
     print "Deleting image of server"
     CNX.images.delete(IMAGE_TYPE)
 
-def copy_exec_script(public_ip, script):
-    ssh_options = ['-q', '-t', '-o StrictHostKeyChecking=no', '-o UserKnownHostsFile=/dev/null']
-    scp_options = ssh_options.remove('-t')
-    
-    tmpname=os.path.join("/tmp", os.path.basename(tempfile.mktemp("-cloudscripts")))
-    call('scp %s %s root@%s:%s' % (" ".join(ssh_options), script, public_ip, tmpname), shell=True)
-    call('ssh -x %s root@%s "%s && rm -f %s"' %  (" ".join(ssh_options), public_ip, tmpname, tmpname), shell=True)
-
 if options.bootstrap:
-    bootstrap_script=os.path.join(os.path.dirname(__file__), "..", "scripts", "bootstrap.sh")
+    bootstrap_script = os.path.join(os.path.dirname(__file__),
+                                    "..", "scripts", "bootstrap.sh")
     copy_exec_script(cstype.public_ip, bootstrap_script)
 
 if options.script:
     copy_exec_script(cstype.public_ip, options.script)
 
-open("/tmp/server-installed-%s.txt" % (os.environ.get("USER", "none")), 'a').write("%s - root@%s -- Password: %s\n" % (cstype.name, cstype.public_ip, cstype.adminPass))
-#open("/tmp/server-installed.txt", 'a').write("%s - root@%s -- Password: %s\n" % (cstype.name, cstype.public_ip, cstype.adminPass))
+open("/tmp/server-installed-%s.txt" % \
+         (os.environ.get("USER", "none")), 'a').write(
+    "%s - root@%s -- Password: %s\n" % (cstype.name,
+                                        cstype.public_ip,
+                                        cstype.adminPass))
 
 if not options.script:
     print
     print
-    print "ssh %s -- Password: %s" % ( cstype.public_ip, cstype.adminPass)
+    print "ssh %s -- Password: %s" % (cstype.public_ip, cstype.adminPass)
---
+++ python/lib/commands.py
@@ -0,0 +1,24 @@
+# -*- encoding: utf-8 -*-
+__author__ = "Chmouel Boudjnah <chmouel@chmouel.com>"
+import os
+import tempfile
+from subprocess import call
+
+
+def copy_exec_script(public_ip, script, username='root'):
+    common_options = ['-q',
+                   '-o StrictHostKeyChecking=no',
+                   '-o UserKnownHostsFile=/dev/null']
+
+    tmpname = os.path.join("/tmp",
+                           os.path.basename(tempfile.mktemp("-cloudscripts")))
+    call('scp %s %s %s@%s:%s' % \
+             (" ".join(common_options), script, username,
+              public_ip, tmpname), shell=True)
+
+    call('ssh -t -x %s %s@%s "%s && rm -f %s"' %  \
+             (" ".join(common_options), username,
+              public_ip, tmpname, tmpname), shell=True)
+
+if __name__ == '__main__':
+    copy_exec_script('IP', '/tmp/a.sh')
